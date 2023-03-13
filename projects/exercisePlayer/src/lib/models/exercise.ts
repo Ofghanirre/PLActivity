37a0be241564a0c6c29ffe6c99d57b9900d88a70
Ofghanirre <aleronraider@gmail.com>
2023-03-13 17:10:12 +0100

    Correction for MathQuill removal

--- projects/exercisePlayer/src/lib/models/exercise.ts
+++ projects/exercisePlayer/src/lib/models/exercise.ts
@@ -1,7 +1,6 @@
 import { CodeEditorForm } from "../forms-player/components/code-editor/code-editor";
 import { FormlyForm } from "../forms-player/components/formlyform/formlyform";
 import { InputBoxForm } from "../forms-player/components/input-box/input-box";
-import { MathLiveForm } from "../forms-player/components/math-live/math-live";
 import { RadioGroupForm } from "../forms-player/components/radio-group/radio-group";
 
 export type markdownString = string;
@@ -13,7 +12,7 @@ export interface FormSuperInterface {
 
 export interface formState {
     selector: string;
-    form: RadioGroupForm | InputBoxForm | CodeEditorForm | FormlyForm | MathLiveForm | FormSuperInterface;
+    form: RadioGroupForm | InputBoxForm | CodeEditorForm | FormlyForm | FormSuperInterface;
     output?: any;
 };
 
