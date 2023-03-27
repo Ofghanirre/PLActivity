2a9c4875983de1ff2a4c643fda34a5dee937427c
Ofghanirre <ofghanirre@gmail.com>
2023-03-27 17:00:31 +0200

    Adding of the CodeEditor.json exercise

--- src/app/loadedJsonExamples.ts
+++ src/app/loadedJsonExamples.ts
@@ -2,8 +2,9 @@ import simpleExercise from '../assets/examples/simpleExercise.json';
 import simpleExercise1 from '../assets/examples/simpleExercise1.json';
 import simpleExerciseCorrection from '../assets/examples/simpleExerciseCorrection.json';
 import inputBox from '../assets/examples/inputBox.json';
+import codeEditor from '../assets/examples/codeEditor.json';
 
 
 export const loadedJsonExamples = [
-  simpleExercise, simpleExercise1, simpleExerciseCorrection, inputBox
+  simpleExercise, simpleExercise1, simpleExerciseCorrection, inputBox, codeEditor
 ]
