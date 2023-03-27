c06a99c1df80eb07db192bba1b564ce55615559d
Ofghanirre <ofghanirre@gmail.com>
2023-03-27 15:36:17 +0200

    Adding of a feedBack json example

--- src/app/loadedJsonExamples.ts
+++ src/app/loadedJsonExamples.ts
@@ -1,7 +1,7 @@
 import simpleExercice from '../assets/examples/simpleExercise.json';
 import simpleExercice1 from '../assets/examples/simpleExercise1.json';
-
+import simpleExerciceCorrection from '../assets/examples/simpleExerciseCorrection.json';
 
 export const loadedJsonExamples = [
-  simpleExercice, simpleExercice1
+  simpleExercice, simpleExercice1, simpleExerciceCorrection
 ]
